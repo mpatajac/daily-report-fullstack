5e2467145ade2f3f0a9b351aad28be3ed5001c80
mpatajac <patajacmatija3@gmail.com>
2020-02-13 12:55:15 +0100

    Add components to program architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { LandingComponent } from './route-components/landing/landing.component';
 import { DashboardComponent } from './route-components/dashboard/dashboard.component';
 import { CreateReportComponent } from './route-components/create-report/create-report.component';
 import { PasswordResetComponent } from './route-components/password-reset/password-reset.component';
+
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ReportsTableComponent } from './components/reports-table/reports-table.component';
+import { ReportElementComponent } from './components/report-element/report-element.component';
+import { WarningComponent } from './components/warning/warning.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +22,13 @@ import { PasswordResetComponent } from './route-components/password-reset/passwo
     LandingComponent,
     DashboardComponent,
     CreateReportComponent,
-    PasswordResetComponent
+    PasswordResetComponent,
+    LoginComponent,
+    HeaderComponent,
+    NavigationComponent,
+    ReportsTableComponent,
+    ReportElementComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/report-element/report-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-element',
+  templateUrl: './report-element.component.html',
+  styleUrls: ['./report-element.component.scss']
+})
+export class ReportElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
