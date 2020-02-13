a9792ddf4daf3462b4c2f0101370a40785a98a88
mpatajac <patajacmatija3@gmail.com>
2020-02-13 12:31:46 +0100

    Add route components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './route-components/landing/landing.component';
+import { DashboardComponent } from './route-components/dashboard/dashboard.component';
+import { CreateReportComponent } from './route-components/create-report/create-report.component';
+import { PasswordResetComponent } from './route-components/password-reset/password-reset.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    DashboardComponent,
+    CreateReportComponent,
+    PasswordResetComponent
   ],
   imports: [
     BrowserModule,
