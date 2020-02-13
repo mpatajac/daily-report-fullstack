ac538d2d47d340c37a5e6bc81edb5b04b96a41ce
mpatajac <patajacmatija3@gmail.com>
2020-02-13 21:24:23 +0100

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LandingComponent } from './route-components/landing/landing.component';
+import { DashboardComponent } from './route-components/dashboard/dashboard.component';
+import { CreateReportComponent } from './route-components/create-report/create-report.component';
+import { PasswordResetComponent } from './route-components/password-reset/password-reset.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'app/dashboard', component: DashboardComponent },
+  { path: 'app/dashboard/:page', component: DashboardComponent },
+  { path: 'app/new', component: CreateReportComponent },
+  { path: 'app/passreset', component: PasswordResetComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
