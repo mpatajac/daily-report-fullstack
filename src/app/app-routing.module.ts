cc5f27fc917955762b72f2239ef18024f25e4b35
mpatajac <patajacmatija3@gmail.com>
2020-02-20 13:37:21 +0100

    Put login instead of landing route component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { LandingComponent } from './route-components/landing/landing.component';
+import { LoginComponent } from './route-components/login/login.component';
 import { DashboardComponent } from './route-components/dashboard/dashboard.component';
 import { CreateReportComponent } from './route-components/create-report/create-report.component';
 import { PasswordResetComponent } from './route-components/password-reset/password-reset.component';
 
 const routes: Routes = [
-  { path: '', component: LandingComponent },
+  { path: '', component: LoginComponent },
   { path: 'app/dashboard', component: DashboardComponent },
   { path: 'app/dashboard/:page', component: DashboardComponent },
   { path: 'app/new', component: CreateReportComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,31 +6,29 @@ import { AppComponent } from './app.component';
 
 // import { BaasicApp } from 'baasic-sdk-angular';
 
-import { LandingComponent } from './route-components/landing/landing.component';
 import { DashboardComponent } from './route-components/dashboard/dashboard.component';
 import { CreateReportComponent } from './route-components/create-report/create-report.component';
 import { PasswordResetComponent } from './route-components/password-reset/password-reset.component';
 
-import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { ReportsTableComponent } from './components/reports-table/reports-table.component';
 import { ReportElementComponent } from './components/report-element/report-element.component';
 import { WarningComponent } from './components/warning/warning.component';
+import { LoginComponent } from './route-components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent,
     DashboardComponent,
     CreateReportComponent,
     PasswordResetComponent,
-    LoginComponent,
     HeaderComponent,
     NavigationComponent,
     ReportsTableComponent,
     ReportElementComponent,
-    WarningComponent
+    WarningComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
