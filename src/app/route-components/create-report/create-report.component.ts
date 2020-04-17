ff01da157b9e21aafcf39a84ffa318e459d990f2
mpatajac <patajacmatija3@gmail.com>
2020-04-17 19:22:59 +0200

    Add routing to 'Submit report'

--- src/app/route-components/create-report/create-report.component.ts
+++ src/app/route-components/create-report/create-report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 import { User } from "../../common/models/user";
 import { UserService } from "../../common/services/user.service";
 
@@ -17,10 +18,20 @@ export class CreateReportComponent implements OnInit {
 
   user: User;
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.user = this.userService.getUser();
+  }
+
+  submitReport() {
+    // TODO: check if any input field is empty, get data
+    if (this.user.showWarning === false) {
+      this.router.navigateByUrl("/app/dashboard")
+    }
   }
 
 }
