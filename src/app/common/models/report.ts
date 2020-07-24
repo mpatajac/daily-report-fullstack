52f29c01327e0ff6c18d2bf421aa481173722ffe
mpatajac <patajacmatija3@gmail.com>
2020-07-24 12:50:59 +0200

    Update Report constructor

--- src/app/common/models/report.ts
+++ src/app/common/models/report.ts
@@ -10,7 +10,20 @@ export class Report {
 	problems: string[];
 	date: Date;
 
-	constructor(user?: User) {
+	constructor(
+		user: User,
+		name: string,
+		done: string[],
+		inProgress: string[],
+		scheduled: string[],
+		problems: string[],
+	) {
 		this.user = user;
+		this.name = name;
+		this.done = done;
+		this.inProgress = inProgress;
+		this.scheduled = scheduled;
+		this.problems = problems;
+		this.date = new Date();
 	}
 }
