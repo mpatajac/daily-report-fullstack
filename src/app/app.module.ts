7178578c975cde2bc5759ec06bc64b60c08444ff
mpatajac <patajacmatija3@gmail.com>
2020-08-24 13:17:22 +0200

    Enable HTTP service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 import { LoginComponent } from './route-components/login/login.component';
@@ -37,6 +38,7 @@ import { WarningComponent } from './components/warning/warning.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
