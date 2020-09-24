3843c686adb5aee0436a7b1d989e737dc3ff1f15
mpatajac <patajacmatija3@gmail.com>
2020-09-24 11:58:02 +0200

    Update ThemeService to bypass circular dependency

--- src/app/common/services/theme.service.ts
+++ src/app/common/services/theme.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { UserService } from './user.service';
 
 @Injectable({
@@ -7,7 +7,7 @@ import { UserService } from './user.service';
 export class ThemeService {
   body: HTMLElement = document.body;
 
-  constructor(private userService: UserService) { }
+  constructor(private injector: Injector) { }
 
   /**
    * Change UI theme to dark (if needed)
@@ -31,7 +31,9 @@ export class ThemeService {
   }
 
   toggleUserThemePreference() {
-    this.userService.toggleThemePreference();
+    // injected to handle circular dependency (false positive)
+    const userService = this.injector.get(UserService);
+    userService.toggleThemePreference();
   }
 
   /**
