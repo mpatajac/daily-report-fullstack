95efa148406b3226f101187100b0dddd993cbb71
mpatajac <patajacmatija3@gmail.com>
2021-03-22 18:18:00 +0100

    Extract DisplayReportComponent as a reuseable component

---
+++ client/src/app/route-components/read-report/read-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-read-report',
+  templateUrl: './read-report.component.html',
+  styleUrls: ['./read-report.component.scss']
+})
+export class ReadReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
