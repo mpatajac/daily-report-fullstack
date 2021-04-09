8e91b8149a13c94a39b78cce52c0d650d41c9f0d
mpatajac <patajacmatija3@gmail.com>
2021-04-09 20:58:22 +0200

    Implement JWT middleware

--- server/src/common/jwt.middleware.js
+++ server/src/common/jwt.middleware.js
@@ -1,1 +1,12 @@
-export function jwtMiddleware() { }
+import jwt from 'jsonwebtoken';
+import config from 'config';
+
+export function jwtMiddleware(req, res, next) {
+	try {
+		const token = req.headers.authorization.slice(7);
+		jwt.verify(token, config.get('jwt.secret'));
+		next();
+	} catch (error) {
+		res.sendStatus(401);
+	}
+}
