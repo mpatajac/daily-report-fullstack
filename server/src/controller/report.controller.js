8dd5833e3f9d826ce317b7c6fae1d458f691fad2
mpatajac <patajacmatija3@gmail.com>
2021-04-09 21:13:03 +0200

    Implement ReportController

--- server/src/controller/report.controller.js
+++ server/src/controller/report.controller.js
@@ -1,10 +1,34 @@
+import { ReportService } from "../service/report.service.js";
+
 export class ReportController {
-	static async get() { }
+	static async get(req, res) {
+		const reports = await ReportService.get();
+		res.send(reports.map(ReportController.#fixId));
+	}
 
-	static async getById() { }
+	static async getById(req, res) {
+		const report = await ReportService.getById(req.params.id);
+		if (!report) {
+			res.sendStatus(404);
+		} else {
+			res.send(ReportController.#fixId(report));
+		}
+	}
 
-	static async create() { }
+	static async create(req, res) {
+		const report = req.body;
+		await ReportService.create(report);
+		res.sendStatus(201);
+	}
 
-	static async upload() { }
+	static async upload(req, res) {
+		// TODO
+	}
+
+	static #fixId(report) {
+		report.id = report._id;
+		delete report._id;
+		return report;
+	}
 
 }
