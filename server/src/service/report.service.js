b05e9e31a368f3c6d4ce1263e62688de2724d3bf
mpatajac <patajacmatija3@gmail.com>
2021-04-09 21:59:17 +0200

    Add declarations so it runs

---
+++ server/src/service/report.service.js
@@ -0,0 +1,10 @@
+export class ReportService {
+	static async get() { }
+	
+	static async getById() { }
+	
+	static async create() { }
+	
+	static async upload() { }
+	
+}
---
+++ server/src/service/user.service.js
@@ -0,0 +1,19 @@
+export class UserService {
+	// Auth
+	
+	static async login() { }
+
+	static async logout() { }
+
+	// ------------------------------------------------
+	// User
+
+	static async get() { }
+
+	static async exists() { }
+
+	static async update() { }
+
+	static async changePassword() { }
+
+}
