68e770ef4c6e9602048cb99b33f1ec0e90fdc3eb
mpatajac <patajacmatija3@gmail.com>
2021-04-09 21:38:24 +0200

    Implement user part of UserController

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -1,13 +1,51 @@
+import { UserService } from "../service/user.service.js";
+
 export class UserController {
-	static async login() { }
+	// Auth
+
+	static async login(req, res) {
+
+	}
 	
-	static async logout() { }
+	static async logout(req, res) {
 
-	static async get() { }
+	}
 
-	static async exists() { }
+	// ------------------------------------------------
+	// User
 
-	static async update() { }
+	static async get(req, res) {
+		const user = await UserService.get(req.params.username);
+		if (!user) {
+			res.sendStatus(404);
+		} else {
+			res.send(user);
+		}
+	}
 
-	static async changePassword() { }
+	static async exists(req, res) {
+		const found = await UserService.exists(req.params.username);
+		const status = found ? 204 : 404;
+		res.sendStatus(status);
+	}
+
+	static async update(req, res) {
+		const updateParams = req.body;
+		try {
+			await UserService.update(updateParams);
+			res.sendStatus(204);
+		} catch (error) {
+			res.sendStatus(error.code);
+		}
+	}
+
+	static async changePassword(req, res) {
+		const newPassword = req.body.newPassword;
+		try {
+			await UserService.changePassword(newPassword);
+			res.sendStatus(200);
+		} catch (error) {
+			res.sendStatus(error.code);
+		}
+	}
 }
