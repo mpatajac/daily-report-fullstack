81c8b81ab7130491632d795e74498e7276d5f987
mpatajac <patajacmatija3@gmail.com>
2021-04-10 12:45:34 +0200

    Start UserService implementation

--- server/src/service/user.service.js
+++ server/src/service/user.service.js
@@ -1,6 +1,11 @@
+import mongodb from "mongodb";
+import { database } from "../common/db.js";
+
 export class UserService {
+	static #users = database.collections("users");
+
 	// Auth
-	
+
 	static async login() { }
 
 	static async logout() { }
@@ -8,9 +13,20 @@ export class UserService {
 	// ------------------------------------------------
 	// User
 
-	static async get() { }
+	static async get(username) {
+		return UserService.#users.findOne({
+			name: username
+		});
+	}
 
-	static async exists() { }
+	static async exists(username) {
+		const user = await UserService.#users.findOne({
+			name: username
+		});
+		
+		// `user` is 'truthy' if a match was found, 'falsy' otherwise
+		return !!user;
+	}
 
 	static async update() { }
 
