a96f1a2cceb7bdff60329484d1eded46cc7503e0
mpatajac <patajacmatija3@gmail.com>
2021-04-20 18:17:47 +0200

    Finish 'auth' part of UserService

--- server/src/service/user.service.js
+++ server/src/service/user.service.js
@@ -1,29 +1,49 @@
-import mongodb from "mongodb";
 import { database } from "../common/db.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import config from "config";
 
 export class UserService {
-	static #users = database.collections("users");
+	static #users = database.collection("users");
 
 	// Auth
 
-	static async login() { }
+	static async login(loginParams) {
+		const { username, password, grant_type } = loginParams;
 
-	static async logout() { }
+		if (grant_type !== "password") {
+			throw { code: 400 };
+		}
+
+		const user = await this.#fetchUser(username);
+		if (!user) {
+			throw { code: 404 };
+		}
+
+		if (!(await bcrypt.compare(password, user.password))) {
+			throw { code: 401 };
+		}
+		return UserService.#signToken(user);
+	}
+
+	static async logout(logoutParams) {
+		const token = logoutParams.token;
+
+		// TODO: add token to `blacklist`
+
+		return true;
+	}
 
 	// ------------------------------------------------
 	// User
 
 	static async get(username) {
-		return UserService.#users.findOne({
-			name: username
-		});
+		return await this.#fetchUser(username);
 	}
 
 	static async exists(username) {
-		const user = await UserService.#users.findOne({
-			name: username
-		});
-		
+		const user = await this.#fetchUser(username);
+
 		// `user` is 'truthy' if a match was found, 'falsy' otherwise
 		return !!user;
 	}
@@ -32,4 +52,19 @@ export class UserService {
 
 	static async changePassword() { }
 
+
+	static async #fetchUser(username) {
+		return await UserService.#users.findOne({
+			name: username
+		});
+	}
+
+	static #signToken(user) {
+		const jwtConfig = config.get('jwt');
+		return jwt.sign(
+			{ id: user._id, username: user.name },
+			jwtConfig.secret,
+			{ expiresIn: jwtConfig.expiresIn }
+		);
+	}
 }
