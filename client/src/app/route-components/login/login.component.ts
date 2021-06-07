28dcaa01945ad7181924fd028b2de9462dd3f308
mpatajac <patajacmatija3@gmail.com>
2021-06-07 17:18:17 +0200

    Add animation to login screen

--- client/src/app/route-components/login/login.component.ts
+++ client/src/app/route-components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm, NgModel } from '@angular/forms';
+import gsap from "gsap";
 import { UserService } from '@app/common/services/user.service';
 
 @Component({
@@ -21,9 +22,9 @@ export class LoginComponent implements OnInit {
     private router: Router,
   ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     // if the user is already logged in, redirect them to dashboard
-    if (this.userService.isLoggedIn()) {
+    if (await this.userService.isLoggedIn()) {
       this.router.navigateByUrl("/app/dashboard");
       return;
     }
@@ -31,6 +32,9 @@ export class LoginComponent implements OnInit {
     // initially set to "true", so the error message doesn't show up
     this.usernameFound = true;
     this.passwordMatches = true;
+
+		// initialize animation
+		this.initAnimation();
   }
 
   /**
@@ -74,4 +78,33 @@ export class LoginComponent implements OnInit {
     );
   }
 
+	
+	initAnimation(): void {
+		const delay = 5;
+		const stepDuration = 1;
+		const toCenter = -200, toLeft = -400;
+		let timeline = gsap.timeline({
+			repeat: -1,
+			delay: delay,
+			repeatDelay: delay,
+			yoyo: false,
+			defaults: {
+				duration: stepDuration,
+				ease: "power3.inOut"
+			}
+		});
+
+		timeline.to("#done1", { xPercent: toLeft }, 0 * delay)
+			.to("#inProgress", { xPercent: toCenter }, 0 * delay)
+
+			.to("#inProgress", { xPercent: toLeft }, 1 * delay)
+			.to("#scheduled", { xPercent: toCenter }, 1 * delay)
+
+			.to("#scheduled", { xPercent: toLeft }, 2 * delay)
+			.to("#problems", { xPercent: toCenter }, 2 * delay)
+
+			.to("#problems", { xPercent: toLeft }, 3 * delay)
+			.to("#done2", { xPercent: toCenter }, 3 * delay)
+
+	}
 }
