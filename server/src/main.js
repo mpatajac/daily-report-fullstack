6c152435fabbf1e83e628c615b541d43354ee94d
mpatajac <patajacmatija3@gmail.com>
2021-06-17 14:06:24 +0200

    Depoly to Heroku

--- server/src/main.js
+++ server/src/main.js
@@ -1,13 +1,25 @@
 import express from "express";
 import config from "config";
+import path from "path";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { apiRouter } from "./router/api.router.js";
 
 (async function () {
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = dirname(__filename);
+
 	const app = express();
 	const port = process.env.PORT || config.get('server.port');
 
 	app.use(express.json());
+	app.use(express.static(path.join(__dirname, '/../public')));
 	app.use('/api', apiRouter);
+
+
+	app.get('*', (_, res) => {
+		res.sendFile(path.join(__dirname, '/../public/index.html'));
+	});
 
 	app.listen(
 		port,
